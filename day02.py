f7f07557b9204b491c74e0bb1b433c231eb9446f
remcmanu <54556405+remcmanu@users.noreply.github.com>
2024-12-09 22:35:22 -0500

    day 2 part 2 complete

--- day02.py
+++ day02.py
@@ -48,78 +48,48 @@ def day02_b(input):
     solid_reports = 0
     for line in file:
         numbers = line.strip().split(' ')
-        increasing = None
-        prev = None
-        valid = True
-        for number in numbers:
-            number = int(number)
-            # second or more iterations
-            if prev != None:
-                distance = number - prev
-                
-                # check if changed by at least one, at most than 3
-                if abs(distance) < 1 or abs(distance) > 3:
-                    prev = number
-                    valid = False
-                    continue
-                
-                # check if increasing / decreasing first time
-                if increasing == None:
-                    increasing = True if distance > 0 else False
-                
-                # check if increasing / decreasing other times
-                if (distance < 0 and increasing) or (distance > 0 and not increasing):
-                    prev = number
-                    valid = False
-                    continue
-
-            
-            prev = number
+        valid = check_valid(numbers)
         if valid:
             solid_reports += 1
         
-        if not valid:
-            print("numbers " + str(numbers))
+        else:
             for i in range(len(numbers)):
                 temp_numbers = list(numbers)
-                print("temp_numbers " + str(temp_numbers))
                 temp_numbers.pop(i)
-                print("numbers " + str(numbers))
-                print(i)
-                increasing = None
-                prev = None
-                valid = True
-                for number in temp_numbers:
-                    print(number)
-                    number = int(number)
-                    # second or more iterations
-                    if prev != None:
-                        distance = number - prev
-                        
-                        # check if changed by at least one, at most than 3
-                        if abs(distance) < 1 or abs(distance) > 3:
-                            prev = number
-                            valid = False
-                            continue
-                        
-                        # check if increasing / decreasing first time
-                        if increasing == None:
-                            increasing = True if distance > 0 else False
-                        
-                        # check if increasing / decreasing other times
-                        if (distance < 0 and increasing) or (distance > 0 and not increasing):
-                            prev = number
-                            valid = False
-                            continue
-
-                    
-                    prev = number
+                valid = check_valid(temp_numbers)
                 if valid:
                     solid_reports += 1
-                    continue
+                    break
     print (solid_reports)
-            
 
+def check_valid(numbers):
+    increasing = None
+    prev = None
+    valid = True
+    for number in numbers:
+        number = int(number)
+        # second or more iterations
+        if prev != None:
+            distance = number - prev
+            
+            # check if changed by at least one, at most than 3
+            if abs(distance) < 1 or abs(distance) > 3:
+                prev = number
+                valid = False
+                continue
+            
+            # check if increasing / decreasing first time
+            if increasing == None:
+                increasing = True if distance > 0 else False
+            
+            # check if increasing / decreasing other times
+            if (distance < 0 and increasing) or (distance > 0 and not increasing):
+                prev = number
+                valid = False
+                continue
+
+        
+        prev = number
+    return valid
 
 day02_b("day02_input.txt")
-
